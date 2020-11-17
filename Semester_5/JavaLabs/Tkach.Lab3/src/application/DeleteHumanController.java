ea7d25b68a7364f43df36fa50a9a1107c2edde15
ChernyHleb <popovarlol@gmail.com>
2020-11-17 15:32:38 +0300

    deleted old controller

--- Semester_5/JavaLabs/Tkach.Lab3/src/application/DeleteHumanController.java
+++ Semester_5/JavaLabs/Tkach.Lab3/src/application/DeleteHumanController.java
@@ -1,5 +1,10 @@
 package application;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class DeleteHumanController implements IController{
-	
+	public Label humanLabel;
+	public Button submitButton,
+		          cancelButton;
 }
