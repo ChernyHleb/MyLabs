aff5d061bbf0c211e8d0265bb4fd3e99d9a38b1e
ChernyHleb <popovarlol@gmail.com>
2020-11-25 10:02:13 +0300

    creating new folder for asm coursework!

---
+++ Semester_5/asmCoursework/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{   int a, b, sum;
+
+    a = 5;
+    b = 6;
+
+    sum = a + b;
+    
+    std::cout << 3 + 1;
+    std::string user;
+    std::cin >> user;
+    std::cout << "Hello World and " << user << std::endl;
+
+    return 0;
+}
