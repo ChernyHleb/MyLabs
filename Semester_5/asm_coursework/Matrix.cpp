fb34df16281df61fba3faa755faf4bd42d6fbc58
ChernyHleb <popovarlol@gmail.com>
2021-02-22 04:25:16 +0300

    tran and cmul implemented

--- Semester_5/asm_coursework/Matrix.cpp
+++ Semester_5/asm_coursework/Matrix.cpp
@@ -75,7 +75,7 @@ Matrix* Matrix::Sum(Matrix* m1, Matrix* m2)
     int lim = m1->GetN() * m1->GetM();
     int* res = new int[lim];
     asm(
-        "loop1:\n\t"
+        "loop_sum:\n\t"
         "dec %%ecx\n\t"
 
         "movl $0, (%%ebx, %%ecx, 4)\n\t"// обнуляем
@@ -85,9 +85,11 @@ Matrix* Matrix::Sum(Matrix* m1, Matrix* m2)
         "add %%eax, (%%ebx, %%ecx, 4)\n\t"// прибавляем элемент 2 массива
         
         "cmp $0, %%ecx\n\t"
-        "jne loop1"
+        "jne loop_sum"
 
-        ::"b"(res), "S"(matrix1), "D"(matrix2), "c"(lim)
+        :
+        :"b"(res), "S"(matrix1), "D"(matrix2), "c"(lim)
+        :
     );
     return new Matrix(res, m1->GetN(), m1->GetM());
 }
@@ -99,7 +101,7 @@ Matrix* Matrix::Sub(Matrix* m1, Matrix* m2)
     int lim = m1->GetN() * m1->GetM();
     int* res = new int[lim];
     asm(
-        "loop2:\n\t"
+        "loop_sub:\n\t"
         "dec %%ecx\n\t"
 
         "movl $0, (%%ebx, %%ecx, 4)\n\t"// обнуляем
@@ -110,9 +112,11 @@ Matrix* Matrix::Sub(Matrix* m1, Matrix* m2)
         "add %%eax, (%%ebx, %%ecx, 4)\n\t"// прибавляем элемент 2 массива
         
         "cmp $0, %%ecx\n\t"
-        "jne loop2"
+        "jne loop_sub"
 
-        ::"b"(res), "S"(matrix1), "D"(matrix2), "c"(lim)
+        :
+        :"b"(res), "S"(matrix1), "D"(matrix2), "c"(lim)
+        :
     );
     return new Matrix(res, m1->GetN(), m1->GetM());
 }
@@ -161,11 +165,76 @@ Matrix* Matrix::Rev(Matrix* m)
 // Умножение матрицы на число
 Matrix* Matrix::CMul(Matrix* m, int c)
 {
+    int* matrix = m->GetMatrix();
+    int lim = m->GetN() * m->GetM();
+    int* res = new int[lim];
+    asm(
+        "loop_cmul:\n\t"
+        "dec %%ecx\n\t"
 
+        "movl $0, (%%edi, %%ecx, 4)\n\t"// обнуляем
+        
+        "movw (%%esi, %%ecx, 4), %%ax\n\t"
+        "movw %3 ,%%bx\n\t"
+        "imulw %%bx\n\t"// результат в dx:ax
+
+        "xor %%ebx, %%ebx\n\t"
+        "movw %%dx, %%bx\n\t"//2 младших байта ebx
+        "shl $8, %%ebx\n\t"// двигаем значение bx в старшие байты ebx
+        "movw %%ax, %%bx\n\t"
+        "movl %%ebx, (%%edi, %%ecx, 4)\n\t"
+
+        "cmp $0, %%ecx\n\t"
+        "jne loop_cmul"
+        :
+        :"D"(res), "S"(matrix), "c"(lim), "m"(c)
+        : 
+    );
+    return new Matrix(res, m->GetN(), m->GetM());
 }
 // Транспонирование
 Matrix* Matrix::Tran(Matrix* m)
 {
+    int* matrix = m->GetMatrix();
+    int N = m->GetN();
+    int M = m->GetM();
+    int* res = new int[N * M];
+    int counter;
+    int lim = N * M;
+    asm(
+        "movl %5, %%ecx\n\t"
+        "loop_tran_m:\n\t"
+        "dec %%ecx\n\t"
+        "movl %%ecx, %4\n\t"// счетчик для перемещения по исходному массиву
 
+        "movl %3, %%edx\n\t"
+        "loop_tran_n:\n\t"
+        "dec %%edx\n\t"
+
+        //счетчик результирующего массива --
+        "movl %5, %%ebx\n\t"
+        "dec %%ebx\n\t" 
+        "movl %%ebx, %5\n\t"
+
+        "movl %4, %%ebx\n\t"
+        "movl (%%esi, %%ebx, 4), %%eax\n\t"
+        "movl %5, %%ebx\n\t"
+        "movl %%eax, (%%edi, %%ebx, 4)\n\t"
+
+        // счетчик исходного массива -= M;
+        "movl %4, %%ebx\n\t"
+        "sub %2, %%ebx\n\t"
+        "movl %%ebx, %4\n\t"
+
+        "cmp $0, %%edx\n\t"
+        "jne loop_tran_n\n\t"
+
+        "cmp $0, %%ecx\n\t"
+        "jne loop_tran_m"
+        :
+        :"D"(res), "S"(matrix), "m"(M), "m"(N), "m"(counter), "m"(lim)
+        : 
+    );
+    return new Matrix(res, M, N);
 }
 
--- Semester_5/asm_coursework/main.cpp
+++ Semester_5/asm_coursework/main.cpp
@@ -14,15 +14,23 @@ void TestSum();
 void PrintArray(int arr[], int len);
 
 int main() {
-    int arr1[6] = {1, 2, 3, 4, 5, 6};
-    int arr2[6] = {1, 1, 1, 0, 0, 0};
+    int arr1[6] = {1, 2, -3, 4, -5, 6};
+    int arr2[6] = {1, 1, -1, 0, 0, 0};
+    int arr3[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
     Matrix* M1 = new Matrix(arr1, 3, 2);
     Matrix* M2 = new Matrix(arr2, 3, 2);
+    Matrix* M3 = new Matrix(arr3, 4, 3);
 
 
-    cout << M1->ToString();
-    cout << M2->ToString();
-    cout <<"SUM M1 + M1\n"<< Matrix::Sum(M1, M1)->ToString();
-    cout <<"SUB M2 - M1\n"<<Matrix::Sub(M2, M1)->ToString();
+    cout <<"M1\n"<< M1->ToString();
+    cout <<"M2\n"<< M2->ToString();
+    cout <<"M3\n"<< M3->ToString();
+    cout << "-------------------------------\n";
+    //cout <<"SUM: M1 + M1\n"<< Matrix::Sum(M1, M1)->ToString();
+    //cout <<"SUB: M2 - M1\n"<< Matrix::Sub(M2, M1)->ToString();
+    //cout << "CMUL: M1 * 3\n" << Matrix::CMul(M1, 3)->ToString();
+    //cout << "CMUL: M1 * -5\n" << Matrix::CMul(M1, -5)->ToString();
+    cout << "TRAN: (M3)T\n" << Matrix::Tran(M3)->ToString();
+    
     getchar();
 }
