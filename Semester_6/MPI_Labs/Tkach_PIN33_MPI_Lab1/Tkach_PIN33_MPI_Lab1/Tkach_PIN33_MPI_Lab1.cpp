91864061a65b21507206951a392a59e46d839a57
ChernyHleb <popovarlol@gmail.com>
2021-03-19 23:33:58 +0300

    mpi lab 2 is done

--- Semester_6/MPI_Labs/Tkach_PIN33_MPI_Lab1/Tkach_PIN33_MPI_Lab1/Tkach_PIN33_MPI_Lab1.cpp
+++ Semester_6/MPI_Labs/Tkach_PIN33_MPI_Lab1/Tkach_PIN33_MPI_Lab1/Tkach_PIN33_MPI_Lab1.cpp
@@ -25,10 +25,7 @@ void DoManagerProcess(int *process_rank,
 				*process_rank, *recv_message, i);
 		}
 		// принимает от каждого сообщение
-		//for (int i = 1; i < *process_amount; i++)
-		//{
-			
-		//}
+		
 		printf("\n");
 	}
 }
@@ -67,7 +64,7 @@ void Star(int repeat)
 	{
 		printf("~ITERATION %d\n", i);
 
-	if (process_rank == 0) {
+		if (process_rank == 0) {
 		/*DoManagerProcess(&process_rank,
 						 &process_amount,
 						 &send_message,
@@ -97,25 +94,25 @@ void Star(int repeat)
 
 			printf("\n");
 		
-	}
-	else {
-		/*DoWorkerProcess(&process_rank,
-						&process_amount,
-						&send_message,
-						&recv_message,
-						&status);*/
-		// процесс принимает рандомное число от основного процесса
-		MPI_Recv(&recv_message, 1, MPI_INT, 0,
-			MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		printf("process %d recv message:'%d' from process %d\n",
-			process_rank, recv_message, 0);
-		// отправка ответа
-		send_message = process_rank;
-		MPI_Send(&send_message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-		printf("Sending message:%d from process %d to %d\n",
-			send_message, process_rank, 0);
+		}
+		else {
+			/*DoWorkerProcess(&process_rank,
+							&process_amount,
+							&send_message,
+							&recv_message,
+							&status);*/
+			// процесс принимает рандомное число от основного процесса
+			MPI_Recv(&recv_message, 1, MPI_INT, 0,
+				MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			printf("process %d recv message:'%d' from process %d\n",
+				process_rank, recv_message, 0);
+			// отправка ответа
+			send_message = process_rank;
+			MPI_Send(&send_message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+			printf("Sending message:%d from process %d to %d\n",
+				send_message, process_rank, 0);
 
-	}
+		}
 
 	}
 
@@ -124,7 +121,62 @@ void Star(int repeat)
 	return;
 }
 
+void DataInitialization(int* arr, int n)
+{
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 100;
+	}
+	return;
+}
+
+void Star_Lab2(int repeat) 
+{
+	int arr[3];
+	int n = 3;
+
+	int process_rank;
+	int process_amount;
+	int send_message;
+	int recv_message;
+	MPI_Status status;
+
+	MPI_Init(NULL, NULL);
+	MPI_Comm_rank(MPI_COMM_WORLD, &process_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &process_amount);
+
+	for (int i = 0; i < 3; i++)
+	{
+		printf("~ITERATION %d\n", i);
+
+		// инициализаци€ массива рандомными числами
+		if (process_rank == 0) 
+			DataInitialization(arr, n);
+
+		// рассылка от 0 процесса всем остальным arr
+		MPI_Bcast(arr, n, MPI_INT, 0, MPI_COMM_WORLD);
+		if (process_rank == 0) 
+			printf("Sending messages from root process to others: %d, %d, %d\n",
+				arr[0], arr[1], arr[2]);
+
+		// каждый процесс что то делает со своей порцией
+		printf("Im process %d! I have %d :)\n", process_rank, arr[process_rank]);
+
+		// все процессы отправл€ют свое сообщение, главному и они суммируютс€
+		send_message = arr[process_rank];
+		MPI_Reduce(&send_message, &recv_message, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+		if (process_rank == 0)
+			printf("Root process recv message: %d\n", recv_message);
+
+
+	}
+
+	MPI_Finalize();
+
+	return;
+}
+
 int main(int argc, char* argv[]) {
-	Star(3);
+	//Star(3);
+	Star_Lab2(2);
 	return 0;
 }
