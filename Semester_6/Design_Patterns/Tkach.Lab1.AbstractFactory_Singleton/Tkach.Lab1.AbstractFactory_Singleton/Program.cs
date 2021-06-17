7495672b846e742f1b5f63fc25a7afa44b11444b
ChernyHleb <popovarlol@gmail.com>
2021-06-17 15:30:43 +0300

    lab1 init

---
+++ Semester_6/Design_Patterns/Tkach.Lab1.AbstractFactory_Singleton/Tkach.Lab1.AbstractFactory_Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tkach.Lab1.AbstractFactory_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!");
+            Console.ReadLine();
+        }
+    }
+}
