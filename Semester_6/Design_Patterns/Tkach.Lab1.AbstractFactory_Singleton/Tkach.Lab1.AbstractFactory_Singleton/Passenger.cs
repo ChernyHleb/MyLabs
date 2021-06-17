f0cd5b5217573c548fb3a9ec1d47b8d46daf7b53
ChernyHleb <popovarlol@gmail.com>
2021-06-17 18:06:05 +0300

    Passenger class implementated

--- Semester_6/Design_Patterns/Tkach.Lab1.AbstractFactory_Singleton/Tkach.Lab1.AbstractFactory_Singleton/Passenger.cs
+++ Semester_6/Design_Patterns/Tkach.Lab1.AbstractFactory_Singleton/Tkach.Lab1.AbstractFactory_Singleton/Passenger.cs
@@ -8,5 +8,17 @@ namespace Tkach.Lab1.AbstractFactory_Singleton
 {
     class Passenger
     {
+        static int counter = 0;
+        string name;
+
+        public Passenger()
+        {
+            this.name = "P" + counter.ToString();
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
     }
 }
