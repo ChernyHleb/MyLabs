24611b11b9052e67def095d2f31e3ae58675fac8
ChernyHleb <popovarlol@gmail.com>
2021-07-28 02:01:58 +0300

    fixing input handle bug and creating maze parser

---
+++ Semester_6/Design_Patterns/Tkach.Lab7-8.Game/Tkach.Lab7-8.Game/MazeParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Tkach.Lab7_8.Game
+{
+    class MazeParser
+    {
+        public static string[,] ParseFileToArray(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            string firstLine = lines[0];
+            int rows = lines.Length;
+            int cols = firstLine.Length;
+            string[,] grid = new string[rows, cols];
+
+            for (int y = 0; y < rows; y++)
+            {
+                string line = lines[y]; 
+                for (int x = 0; x < cols; x++)
+                {
+                    char sym = line[x];
+                    if(sym == '|' || sym == '-' || sym == '+')
+                    {
+                        sym = '█';
+                    }
+                    grid[y, x] = sym.ToString();
+                }
+            }
+
+            return grid;
+        }
+    }
+}
