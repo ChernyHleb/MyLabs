e049f368a5dffc369b33ce369503eda287b911e5
ChernyHleb <popovarlol@gmail.com>
2021-12-07 21:12:05 +0300

    refactor track

--- Semester_6/PPPP/PPPP.Tkach.Lab3/PPPP.Tkach.Lab3/Track.cpp
+++ Semester_6/PPPP/PPPP.Tkach.Lab3/PPPP.Tkach.Lab3/Track.cpp
@@ -12,46 +12,10 @@ Track::~Track()
 
 }
 
-Track::Track(Car car1, int volume)
+void Track::AddVehicle(Vehicle vehicle, int volume)
 {
-	cost = car1.sumCost(volume, car1.getDistance());
-	time = car1.sumTime(car1.getDistance());
-}
-
-Track::Track(Car car1, Car car2, Train train1, int volume)
-{
-	cost = car1.sumCost(volume, car1.getDistance()) + car2.sumCost(volume, car2.getDistance());
-	time = car1.sumTime(car1.getDistance()) + car2.sumTime(car2.getDistance());
-	cost += train1.sumCost(volume, train1.getDistance());
-	time += train1.sumTime(train1.getDistance());
-}
-
-Track::Track(Car car1, Car car2, Plane plane1, int volume)
-{
-	cost = car1.sumCost(volume, car1.getDistance()) + car2.sumCost(volume, car2.getDistance());
-	time = car1.sumTime(car1.getDistance()) + car2.sumTime(car2.getDistance());
-	cost += plane1.sumCost(volume, plane1.getDistance());
-	time += plane1.sumTime(plane1.getDistance());
-}
-
-Track::Track(Car car1, Car car2, Car car3, Train train1, Plane plane1, int volume)
-{
-	cost = car1.sumCost(volume, car1.getDistance()) + car2.sumCost(volume, car2.getDistance()) + car3.sumCost(volume, car3.getDistance());
-	time = car1.sumTime(car1.getDistance()) + car2.sumTime(car2.getDistance()) + car3.sumTime(car3.getDistance());
-	cost += train1.sumCost(volume, train1.getDistance());
-	time += train1.sumTime(train1.getDistance());
-	cost += plane1.sumCost(volume, plane1.getDistance());
-	time += plane1.sumTime(plane1.getDistance());
-}
-
-Track::Track(Car car1, Car car2, Car car3, Car car4, Train train1, Train train2, Plane plane1, int volume)
-{
-	cost = car1.sumCost(volume, car1.getDistance()) + car2.sumCost(volume, car2.getDistance()) + car3.sumCost(volume, car3.getDistance()) + car4.sumCost(volume, car4.getDistance());
-	time = car1.sumTime(car1.getDistance()) + car2.sumTime(car2.getDistance()) + car3.sumTime(car3.getDistance()) + car4.sumTime(car4.getDistance());
-	cost += train1.sumCost(volume, train1.getDistance()) + train2.sumCost(volume, train2.getDistance());
-	time += train1.sumTime(train1.getDistance()) + train2.sumTime(train2.getDistance());
-	cost += plane1.sumCost(volume, plane1.getDistance());
-	time += plane1.sumTime(plane1.getDistance());
+	cost += vehicle.sumCost(volume, vehicle.getDistance());
+	time += vehicle.sumTime(vehicle.getDistance());
 }
 
 double Track::getCost()
--- Semester_6/PPPP/PPPP.Tkach.Lab3/PPPP.Tkach.Lab3/Track.h
+++ Semester_6/PPPP/PPPP.Tkach.Lab3/PPPP.Tkach.Lab3/Track.h
@@ -1,7 +1,6 @@
 #pragma once
-#include "../PPPP.Tkach.Lab3/Car.h"
-#include "../PPPP.Tkach.Lab3/Plane.h"
-#include "../PPPP.Tkach.Lab3/Train.h"
+#include "../PPPP.Tkach.Lab3/Vehicle.h"
+
 class Track
 {
 	double cost;
@@ -9,11 +8,7 @@ class Track
 	int volume;
 public:
 	Track();
-	Track(Car, int);
-	Track(Car, Car, Train, int);
-	Track(Car, Car, Plane, int);
-	Track(Car, Car, Car, Train, Plane, int);
-	Track(Car, Car, Car, Car, Train, Train, Plane, int);
+	void AddVehicle(Vehicle, int);
 	double getCost();
 	~Track();
 };
