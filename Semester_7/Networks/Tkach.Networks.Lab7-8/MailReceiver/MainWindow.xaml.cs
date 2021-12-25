f75a6a30ed654079c322cbdd6e305037d742f16d
ChernyHleb <popovarlol@gmail.com>
2021-12-25 22:31:59 +0300

    Lab 7-8

---
+++ Semester_7/Networks/Tkach.Networks.Lab7-8/MailReceiver/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Windows;
+using MailKit.Net.Pop3;
+using MailKit;
+using MimeKit;
+
+
+namespace MailReceiver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Pop3Client client;
+        public MainWindow()
+        {
+            client = new MailKit.Net.Pop3.Pop3Client();
+            InitializeComponent();
+        }
+        public void ClickRecieveButton(object sender, RoutedEventArgs e)
+        {
+            
+                client.Connect("pop.mail.ru", 995, true);
+
+                client.Authenticate("tkach@internet.ru", "tUsrFijviHXvSzyQmnvc");
+
+                for (int i = 0; i < client.Count; i++)
+                {
+                    var message = client.GetMessage(i);
+                    tb_text.Text += "Subject:" +  message.Subject;
+                }
+
+                client.Disconnect(true);
+            
+        }
+    }
+}
---
+++ Semester_7/Networks/Tkach.Networks.Lab7-8/MailSender/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using MailKit;  
+using MailKit.Net.Smtp;
+using MimeKit;
+
+
+
+namespace MailSender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MimeMessage message;
+        SmtpClient client;
+        public MainWindow()
+        {
+            message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Никита Ткач", "tkach@internet.ru"));
+            message.Subject = "From MailSender";
+            client = new SmtpClient();
+            InitializeComponent();
+        }
+
+        public void ClickSendButton(object sender, RoutedEventArgs e)
+        {
+            l_err.Text = "";
+            try
+            {            
+                
+                if (tb_adress.Text == "")
+                    throw new Exception("No adress");                            
+                if (tb_text.Text == "")
+                    throw new Exception("No adress");
+                message.To.Add(new MailboxAddress("Никита Ткач", tb_adress.Text));
+                message.Body = new TextPart("plain")
+                {
+                    Text = tb_text.Text
+                };
+
+                    client.Connect("smtp.mail.ru", 465, true);
+
+                    // Note: only needed if the SMTP server requires authentication
+                    client.Authenticate("tkach@internet.ru", "tUsrFijviHXvSzyQmnvc");
+
+                    client.Send(message);
+                    client.Disconnect(true);
+            }
+            catch(Exception ex)
+            {
+                l_err.Text = ex.Message;
+            }
+        }
+    }
+}
